30d8b033f311ae95be772fc82eb43d3019cbd6cb
sidification <ahuja12@illinois.edu>
2023-08-04 10:08:16 -0500

    Dragleave and dragexit events turn boundary solid and dashed

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
 // select the input and the drop zone element
-document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-const dropZoneElement = inputElement.closest(".drop-zone");
+document.querySelectorAll(".drop-zone--input").forEach(inputElement => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
 
-// now that the elements have been selected, add event listeners
-dropZoneElement.addEventListener('dragover', e => {
-    dropZoneElement.classList.add("drop-zone--over")
-})
-}
+    // now that the elements have been selected, add event listeners
+    dropZoneElement.addEventListener('dragover', e => {
+        dropZoneElement.classList.add("drop-zone--over")
+    });
+
+    ["dragleave", "dragend"].forEach(type => {
+        dropZoneElement.addEventListener(type, e => {
+            dropZoneElement.classList.remove('drop-zone--over')
+
+        })
+    })
+    }
 )
